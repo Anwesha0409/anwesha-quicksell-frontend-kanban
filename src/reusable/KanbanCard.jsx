195958c6cda17a34a47302f0eb9af994c7c084bd
Anwesha Sahu <anwesha.sahu0409@gmail.com>
2024-11-05 01:45:47 +0530

    updated code structure

--- src/reusable/KanbanCard.jsx
+++ src/reusable/KanbanCard.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import "./KanbanCard.css"
+// KanbanCard.jsx
+import React from 'react';
+import "./KanbanCard.css";
 
-const KanbanCard = () => {
+const KanbanCard = ({ ticket, users, toggleTaskCompletion }) => {
+  const user = users.find((user) => user.id === ticket.userId);
+
   return (
-    <div>KanbanCard</div>
-  )
-}
+    <div className="kanban-card">
+      <div className="card-header">
+        {/* Checkbox to toggle task completion */}
+        <input
+          type="checkbox"
+          className="task-checkbox"
+          checked={ticket.status === "Completed"}
+          onChange={() => toggleTaskCompletion(ticket.id)}
+        />
+        <span className="ticket-id">{ticket.id}</span>
 
-export default KanbanCard
+        {/* User avatar in the top right */}
+        {user && (
+          <div className="user-avatar">
+            <img src={user.profilePictureUrl} alt={user.name} />
+          </div>
+        )}
+      </div>
+
+      {/* Card content */}
+      <h4>{ticket.title}</h4>
+
+      {/* Ticket tags */}
+      <div className="ticket-tags">
+        {ticket.tag.map((tag, idx) => (
+          <div key={idx} className="ticket-tag">
+            <p className='ticket-tag-name'>⚪ {tag}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default KanbanCard;
